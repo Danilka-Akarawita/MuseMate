70f17f18f1a2d431993ef3bb6012501ae794b29d
Danilka-Akarawita <danilka.20210514@iit.ac.lk>
2023-04-09 08:51:09 +0530

    feat:added -responsive navbar

---
+++ MuseMate/src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import './NavbarStyles.css';
+
+
+export default function Navbar() {
+  const navRef = useRef();
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle(
+			"responsive_nav"
+		);
+	};
+  return (
+    
+    <header>
+      <h3>Logo</h3>
+        <nav ref={navRef}>
+          
+            <a href="#">Home</a>
+            <a href="#">About</a>
+           <a href="#">Contact</a>
+          <button
+              className="nav-btn nav-close-btn"
+              onClick={showNavbar}>
+              <FaTimes />
+				</button>
+        </nav>
+        <button
+          className="nav-btn"
+          onClick={showNavbar}>
+          <FaBars />
+			</button>
+    </header>
+
+      
+        
+       
+  )
+}
