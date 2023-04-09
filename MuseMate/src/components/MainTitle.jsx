e40828d279f691889194c727de6be63e7a932f7f
Danilka-Akarawita <danilka.20210514@iit.ac.lk>
2023-04-09 19:20:23 +0530

    tailwind

--- MuseMate/src/components/MainTitle.jsx
+++ MuseMate/src/components/MainTitle.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import "./title.css"
 
 export default function MainTitle() {
   return (
     <>
-    <div className="pt-10">MuseMate</div>
-    <div className="">"Make musical bookings a breeze with MuseMate"</div>
+    <div className="wrapper">
+    <div className="bg-slate-700">MuseMate</div>
+    <div className="dynamicText">"Make musical bookings a breeze with MuseMate"</div>
+    </div>
     </>
   )
 }
