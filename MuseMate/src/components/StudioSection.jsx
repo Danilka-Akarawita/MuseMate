3b32024d8a1bc8ba7e3e39727a0843943e5cf402
Danilka-Akarawita <danilka.20210514@iit.ac.lk>
2023-04-11 21:23:09 +0530

    feat: added studioSection

---
+++ MuseMate/src/components/StudioSection.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './studiosection.css'
+import { NavLink } from 'react-router-dom'
+
+
+export default function StudioSection() {
+  return (
+    <div className="ui-card">
+        <img
+         className="m-4 object-cover rounded-lg"
+          src='./images/Mic.png' alt='Studio 1'></img>
+          <div className="description">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <div className="  space-x-4 mt-4 justify-center  flex flex-row">
+            <NavLink to="/signup">
+                <p>Book!</p>
+           </NavLink>
+           <NavLink to="/signup">
+                <p>Details</p>
+           </NavLink>
+            </div>
+            
+          </div>
+    </div>
+  )
+}
--- MuseMate/src/pages/Home.jsx
+++ MuseMate/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../components/navbar'
 import SignUp from './SignUp'
+import StudioSection from '../components/StudioSection'
 
 
 
@@ -8,10 +9,12 @@ export default function Home() {
   return (
     <>
    <Navbar/>
+   
    <div className=" m-16">
    <div className="font-lemon text-5xl ">MuseMate</div>
     <div className="font-lemon  ">Make musical bookings a breeze with MuseMate</div>
     </div>
+    <StudioSection/>
     
    </>
     
